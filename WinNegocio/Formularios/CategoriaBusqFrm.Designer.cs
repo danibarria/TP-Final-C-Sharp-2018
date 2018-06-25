233bf3dba4acb8e535927dc54fd9db85acb4a8b3
dani barria <barriareyesjosedaniel@gmail.com>
2018-06-25 19:24:16 -0300

    listado categoria

--- WinNegocio/Formularios/CategoriaBusqFrm.Designer.cs
+++ WinNegocio/Formularios/CategoriaBusqFrm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.listaTodo = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listaTodo
@@ -43,13 +44,24 @@
             this.listaTodo.UseVisualStyleBackColor = true;
             this.listaTodo.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
-            // Form1
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(160, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CategoriaBusqFrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(467, 224);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.listaTodo);
-            this.Name = "Form1";
+            this.Name = "CategoriaBusqFrm";
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -59,5 +71,6 @@
         #endregion
 
         private System.Windows.Forms.CheckBox listaTodo;
+        private System.Windows.Forms.Button button1;
     }
 }
